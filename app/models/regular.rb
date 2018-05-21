33286fad55f356ca3ea9f91a88ebd114b45cd44d
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-05-21 18:44:50 -0400

    add associations for rest of tables. untested.

--- app/models/regular.rb
+++ app/models/regular.rb
@@ -1,4 +1,4 @@
 class Regular < ApplicationRecord
   belongs_to :tender
-  belongs_to :user
+  belongs_to :customer, foreign_key: "user_id", class_name: "User"
 end
--- app/models/tender.rb
+++ app/models/tender.rb
@@ -1,4 +1,6 @@
 class Tender < User
   has_many :schedules
-  has_one :location, as: :locatable 
+  has_many :regulars
+  has_many :customers, through: :regulars, foreign_key: "user_id", class_name: "User"
+  has_many :venues, through: :schedules 
 end
