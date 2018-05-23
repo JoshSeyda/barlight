d2b8c218bf7756a380db47b145ecbf81cda5c68a
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-05-22 22:04:02 -0400

    BI-DIRECTIONAL LOOPED ASSOCIATION, HAS_MANY-TO-HAS_MANY SELF JOIN BLAAAAAAAHHHHHH

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,19 @@ class User < ApplicationRecord
   has_many :user_roles
   has_many :roles, through: :user_roles
 
-  has_many :regulars
-  has_many :tenders, through: :regulars, class_name: "User"
-  has_many :customers, through: :regulars, class_name: "User"
   has_many :venues, through: :schedules 
   has_many :schedules
+
+  # regular_tenders "names" the Regular join table for accessing through the tender association
+  has_many :regular_tenders, foreign_key: :tender_id, class_name: "Regular" 
+  # source: :customer matches with the belong_to :customer identification in the Regular model 
+  has_many :tenders, through: :regular_tenders, source: :customer
+
+  # regular_customers "names" the Regular join table for accessing through the customer association
+  has_many :regular_customers, foreign_key: :customer_id, class_name: "Regular"    
+  # source: :customer matches with the belong_to :customer identification in the Regular model   
+  has_many :customers, through: :regular_customers, source: :tender
+ 
 
   def assign_default_role
     self.add_role(:customer)
