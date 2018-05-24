fe67d4c5a7ebe571b0dc6fb0b1daf13036d25bca
[01;31m[K"name"[m[K: "Alexandra Cooper <allie.g.cooper@gmail.com>
2018-05-24 17:05:15 -0400

    add custom markers to this repo. refactor google maps init to expect data as formatted by backend. test.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @default_location = Venue.find(2).venue_marker_data
+    @markers = User.first.get_locations
     #Alex Stuff:
     #Venue.near([40.71, -100.23], 50) will show the nearby locations and only pull those. Won't be helpful until we are refreshing the map with AJAX?
     #ie: open page, show results near default location (nycda for now), then once/if user location is given, send user location via AJAX call to refresh the map
@@ -18,6 +20,11 @@ class UsersController < ApplicationController
   end
     
     def show
+      @default_location = Venue.find(2).venue_marker_data
+      @user = User.find(params[:id])
+      @markers = @user.get_locations
+      puts @user 
+      puts @markers 
     end
 
     def new
