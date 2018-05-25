ae8e93a782898c37bd25dde53df16fc802ca8333
babiesinspace <allie.g.cooper@gmail.com>
2018-05-24 23:03:29 -0400

    implement search bar. query users table for handle or venue table for title. add for user full name? write helper method on venue model to find all the users who work there (ie: those who have scheduled work events there. post-mvp: verified by bar owners)

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :events
   resources :recurring_events
   resources :images
+  get '/search' => 'regulars#search', :as => 'search_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
