b47ce005b9891219bd292a90029a43e18e9beb0e
babiesinspace <allie.g.cooper@gmail.com>
2018-05-25 14:58:14 -0400

    Add line to only create new regular instance if user is not already following

--- app/controllers/regulars_controller.rb
+++ app/controllers/regulars_controller.rb
@@ -38,16 +38,18 @@ class RegularsController < ApplicationController
   def create
     customer = User.find(current_user.id)
     tender = User.find(params[:id])
-    @following = Regular.create(tender_id: tender.id, customer_id: customer.id)
-    if @following.save
-      # Handle a successful creation
-      respond_to do |format|
-        format.html { redirect_to user_regulars_path(current_user.id)}
-        format.js
+    if Regular.find_by(tender_id: tender.id, customer_id: customer.id).empty?
+      @following = Regular.create(tender_id: tender.id, customer_id: customer.id)
+      if @following.save
+        # Handle a successful creation
+        respond_to do |format|
+          format.html { redirect_to user_regulars_path(current_user.id)}
+          format.js
+        end
+      else
+        render 'search'
       end
-    else
-      render 'search'
-    end
+    end 
   end
 
   # def update
